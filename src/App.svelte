834c2da4e012a4abb8a8198330c82430f9eded4e
Japatius <Japatius_96@hotmail.com>
2021-01-22 14:54:49 +0200

    dev branch

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Container from "./components/Container.svelte";
   export let name: string;
   export let numbar: number;
 </script>
@@ -6,30 +7,9 @@
 <main>
   <h1>Hello {name}!</h1>
   <h4>de numbar is {numbar}</h4>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  <Container />
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
+  /** */
 </style>
---
+++ src/components/Container.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<main>
+  <p>I am a component</p>
+</main>
+
+<style>
+  /** */
+</style>
