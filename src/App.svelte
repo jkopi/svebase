9029676230d94b2adfc3ab0f482656501b095cec
Japatius <Japatius_96@hotmail.com>
2021-02-05 22:51:45 +0200

    fixed indentations mby

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,42 @@
 <script lang="ts">
-  import { Router, Link, Route, link } from "svelte-routing";
+  import {Router, Link, Route, link} from "svelte-routing";
   import Container from "./components/Container.svelte";
   import Navigation from "./components/Navigation.svelte";
-  import { auth, signIn, signOut } from "./config/firebase";
-  import { authState } from "rxfire/auth";
+  import {auth, signIn, signOut} from "./config/firebase";
+  import {authState} from "rxfire/auth";
   import Home from "./views/Home.svelte";
   import LoginButton from "./components/LoginButton.svelte";
   import NotFound from "./views/NotFound.svelte";
   import Icon from "./components/Icon.svelte";
-  import { faHome } from '@fortawesome/free-solid-svg-icons'
+  import {faHome} from '@fortawesome/free-solid-svg-icons'
+
   let user;
 
   const unsubscribe = authState(auth).subscribe((u) => (user = u));
 </script>
 
 <Router>
-  <Navigation>
-    <a href="/" class="link" use:link>
-      <Icon icon={faHome} iconSize="2x" iconColor="orange"/>
-    </a>
-    {#if user}
-      <button on:click={() => signOut()}>logout</button>
-    {:else}
-      <LoginButton on:login={signIn} />
-    {/if}
-  </Navigation>
-  <Container>
-    <Route path="/">
-      <Home/>
-    </Route>
-    <Route component="{NotFound}"/>
-  </Container>
+	<Navigation>
+		<a href="/" class="link" use:link>
+			<Icon icon={faHome} iconSize="2x" iconColor="orange"/>
+		</a>
+		{#if user}
+			<button on:click={() => signOut()}>logout</button>
+		{:else}
+			<LoginButton on:login={signIn}/>
+		{/if}
+	</Navigation>
+	<Container>
+		<Route path="/">
+			<Home/>
+		</Route>
+		<Route component="{NotFound}"/>
+	</Container>
 </Router>
 
 <style>
-  .link {
-    color: black;
-    text-decoration: none;
-  }
+    .link {
+        color: black;
+        text-decoration: none;
+    }
 </style>
--- src/components/RecipeList/List.svelte
+++ src/components/RecipeList/List.svelte
@@ -1,58 +1,58 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+  import {onMount} from "svelte";
 
-    let dataz = [
+  let dataz = [
+    {
+      name: "omelette with paprika",
+      ingredients: [
         {
-            name: "omelette with paprika",
-            ingredients: [
-                {
-                    quantity: 4,
-                    volume: "whole",
-                    ingredient: "Egg",
-                },
-                {
-                    quantity: 1,
-                    volume: "whole",
-                    ingredient: "Paprika",
-                },
-                {
-                    quantity: 1,
-                    volume: "whole",
-                    ingredient: "Onion",
-                },
-            ],
-            description: "easy omelette with paprika and onions",
-            createdAt: new Date(),
-            uid: "kaytid",
+          quantity: 4,
+          volume: "whole",
+          ingredient: "Egg",
         },
-				{
-            name: "Meat stew",
-            ingredients: [
-                {
-                    quantity: 4,
-                    volume: "whole",
-                    ingredient: "Egg",
-                },
-                {
-                    quantity: 1,
-                    volume: "whole",
-                    ingredient: "Paprika",
-                },
-                {
-                    quantity: 1,
-                    volume: "whole",
-                    ingredient: "Onion",
-                },
-            ],
-            description: "easy omelette with paprika and onions",
-            createdAt: new Date(),
-            uid: "kaytid",
-        }
-    ];
+        {
+          quantity: 1,
+          volume: "whole",
+          ingredient: "Paprika",
+        },
+        {
+          quantity: 1,
+          volume: "whole",
+          ingredient: "Onion",
+        },
+      ],
+      description: "easy omelette with paprika and onions",
+      createdAt: new Date(),
+      uid: "kaytid",
+    },
+    {
+      name: "Meat stew",
+      ingredients: [
+        {
+          quantity: 4,
+          volume: "whole",
+          ingredient: "Egg",
+        },
+        {
+          quantity: 1,
+          volume: "whole",
+          ingredient: "Paprika",
+        },
+        {
+          quantity: 1,
+          volume: "whole",
+          ingredient: "Onion",
+        },
+      ],
+      description: "easy omelette with paprika and onions",
+      createdAt: new Date(),
+      uid: "kaytid",
+    }
+  ];
 
-    onMount(() => {
-        console.log(JSON.stringify(dataz))
-    });
+  onMount(() => {
+    console.log(JSON.stringify(dataz))
+  });
 </script>
 
 <ul>
